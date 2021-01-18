f4c83c5fbd66a568fa41627c9a38877fb743ccf8
Jordan Powers <jordanpowers1227@gmail.com>
2021-01-17 20:32:01 -0800

    [milestone0] Keep build dir empty

--- assignment/milestone0/sync.sh
+++ assignment/milestone0/sync.sh
@@ -6,9 +6,13 @@ ORIG_DIR=$(pwd)
 
 cd $(dirname "$0")
 
+[ ! -d build ] && mkdir build
+
 python3 ./gen.py
 
 ssh -t jjpowers@unix3.csc.calpoly.edu "rm -f /home/jjpowers/Documents/CSC-305/milestone0/*"
 scp -r build/* jjpowers@unix3.csc.calpoly.edu:/home/jjpowers/Documents/CSC-305/milestone0/
+
+rm -r build
 
 cd ${ORIG_DIR}
